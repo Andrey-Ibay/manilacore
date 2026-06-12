30bec630a95140b7993820fba931cc75e880bbd4
Andrey Ibay <andrey.ibay.p@gmail.com>
2026-06-13 02:15:31 +0800

    functional management of data buckets and storage, fix rendering

--- src/app/(user)/profile/page.jsx
+++ src/app/(user)/profile/page.jsx
@@ -1,49 +1,80 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import LogoutButton from "@/components/LogoutButton";
 import { createClient } from "@/utils/supabase/client";
-import { ImageResponse } from 'next/server';
 
 export default function ProfilePage(){
     const supabase = createClient();
-    
-    const handleUpload = async (event) =>{
-        const [image, setImage] = useState([]);
-        const file = event.target.files[0];
-        const filePath = `postImages/${Date.now()}_${file.name}`;
+
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const { data } = await supabase.from('items').select('*');
+            console.log("Stored on state : ", [...images, ...data.map(datum => datum)]);
+            setImages(images => [...images, ...data.map(datum => datum)]);
+        };
+        fetchData();
+    }, []);
+
+    const uploadImage = async (file) => {
+        //formats file
+        const fileExtension = file.name.split('.').pop();
+        const fileName = `${Math.random()}.${fileExtension}`;
+        const filePath = `postImages/${fileName}`;
 
         //uploads image to storage bucket
         const { data, error } = await supabase.storage
             .from("postImages")
             .upload(filePath, file);
+
         if(error){
-            console.log("Upload failed:", error);
-            return;
+            console.log("Failed to upload file: ", error);
         }
 
-        const { data: {user} } = await supabase.auth.getUser();
-        //Actually saves to the db table
-        const { error: dbError } = await supabase
-            .from("images")
-            .update({ image_path: filepath })
-            .eq('user_id', user.id);
-
-        //Fetch the saved data
-        const { data: userRecord } = await supabase
-            .from('users')
-            .select('image_path')
-            .eq('id', user.id)
-            .single();
-
-        const { data: publicUrlData } = supabase.storage
-            .from('postImages')
-            .getPublicUrl(userRecord.image_path);
-
-        //Rendering to state
-        setImage(...image, `${publicUrlData}`);
+        return filePath;
     }
+    const saveToDatabase = async (filePath, imageName) => {
+        const { data, error } = await supabase
+            .from("items")
+            .insert([
+                {
+                    image_path: filePath
+                }
+            ]);
+
+        if (error) console.log("Failed to save to database: ", error);
+    }
+    const handleUpload = async (event) =>{
+        console.log("This triggered.");
+        const file = event.target.files[0];
+
+        if(file){
+            try{
+                const filePath = await uploadImage(file);
+
+                await saveToDatabase(filePath, file.name);
+                console.log("Success");
+
+            }catch (error){
+                console.error("Upload failed: ", error);
+            }
+        }else{
+            console.log("Something went wrong.");
+        }
+
+    }
+    const getImageUrl = (filePath) => {
+        const { data } = supabase.storage
+            .from('postImages')
+            .getPublicUrl(filePath);
+        
+        return data.publicUrl;
+    }
+
+    
     return(
         <div className="min-h-screen flex items-center justify-center">
             <h1>
@@ -52,11 +83,12 @@ export default function ProfilePage(){
             <LogoutButton />
             <input type="file" onChange={handleUpload}/>
             <div>
+                {images.map((row) => (
+                    <div key={row.id}>
+                        <img src={getImageUrl(row.image_path)} className="w-20 h-20"/>
+                    </div>
+                ))}
             </div>
-            {image.map((img, index) => {
-                <img src={`${publicUrlData}`} key={index}/>
-            })}
-                
         </div>
     );
 }
